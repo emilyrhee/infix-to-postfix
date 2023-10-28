1e68c25fa10859f2e2941f0ae8a070f3145a09c0
emilyrhee <emilyrhee99@gmail.com>
2023-10-28 15:56:02 -0400

    added priority()

--- Main.java
+++ Main.java
@@ -1,12 +1,27 @@
 import java.util.Arrays;
 
 class Main {
+    static int priority(String s) {
+        switch (s) {
+            case "+":
+            case "-":
+                return 1;
+
+            case "*":
+            case "/":
+                return 2;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         String infixExp = "13 + 5"; // elements must be separated by space
 
         String[] infixArray = infixExp.split(" ");
 
         Queue infixQueue = new Queue();
+        Stack operators = new Stack();
+        Queue postfixQueue = new Queue();
 
         for (int i = 0; i < infixArray.length; i++) {
             infixQueue.enqueue(infixArray[i]);
