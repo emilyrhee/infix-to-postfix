dbd285e20c2b0b519f2a6c04aa1f1ca7c79cda8b
emilyrhee <emilyrhee99@gmail.com>
2023-10-28 11:58:01 -0400

    added Stack class

---
+++ Stack.java
@@ -0,0 +1,44 @@
+class Stack {
+    private Node top;
+    private int size;
+
+    public LLStackADT() {
+        top = null;
+        size = 0;
+    }          
+
+    public boolean empty() {
+        return (top == null);
+    }
+
+    public void push (int number) {
+        Node newNode = new Node(number, top);
+
+        top = newNode;
+
+        size++;     
+    }
+
+    public int pop () {
+        int i;
+
+        i = top.getData();
+        top = top.getNext();
+
+        size--;
+
+        return i;
+    }
+
+    public int ontop() {
+        int i = pop();
+
+        push(i);
+
+        return i;
+    }
+
+    public int size() {
+        return size;
+    }
+}
