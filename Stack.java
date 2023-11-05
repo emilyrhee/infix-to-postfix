3dbc5bdc2559a265023ce03f7d3b084a03cf83ce
emilyrhee <emilyrhee99@gmail.com>
2023-11-05 15:38:34 -0500

    removed Node class... how did it get there??

--- Stack.java
+++ Stack.java
@@ -41,21 +41,3 @@ public class Stack<T> {
         System.out.println();
     }    
 }
-
-class Node<T> {
-    private T data;
-    private Node<T> next;
-
-    public Node(T data, Node<T> next) {
-        this.data = data;
-        this.next = next;
-    }
-
-    public T getData() {
-        return data;
-    }
-
-    public Node<T> getNext() {
-        return next;
-    }
-}
